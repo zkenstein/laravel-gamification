374dcebc3861e82df3c5f443aa5e265c3d38ea0d
pascalbaljet <pascal@pascalbaljet.nl>
2016-03-09 20:42:24 +0100

    Migrations

--- tests/PointsServiceTest.php
+++ tests/PointsServiceTest.php
@@ -8,10 +8,20 @@ class PointsServiceTest extends TestCase
     {
         parent::setUp();
 
-        app(CreateEarnersTable::class)->up();
-        app(CreateRewardersTable::class)->up();
-        app(CreateItemsTable::class)->up();
-        app(CreateGamificationPointsTable::class)->up();
+        $this->setUpDatabase();
+    }
+
+    protected function setUpDatabase()
+    {
+        include_once __DIR__ . '/../resources/migrations/create_gamification_points_table.php.stub';
+        include_once __DIR__ . '/create_earners_table.php.stub';
+        include_once __DIR__ . '/create_items_table.php.stub';
+        include_once __DIR__ . '/create_rewarders_table.php.stub';
+
+        (new \CreateGamificationPointsTable)->up();
+        (new \CreateEarnersTable)->up();
+        (new \CreateRewardersTable)->up();
+        (new \CreateItemsTable)->up();
     }
 
     public function a_rewarder_can_give_points_to_a_earner_with_an_item()
